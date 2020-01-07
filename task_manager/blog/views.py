cca41663cb08048b6ef03e9d7ebc3ab0c3bfbc0c
johnson <johnsonhao@js.local>
2020-01-06 21:27:24 -0500

    add more css and optimize routing

--- task_manager/blog/views.py
+++ task_manager/blog/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
+
+posts = [
+	{
+		'author': 'Johnson',
+		'title': 'Blog Post 1',
+		'content': 'First Post Content',
+		'date_posted': 'Jan 05, 2020'
+	},
+	{
+		'author': 'Johnson2',
+		'title': 'Blog Post 2',
+		'content': 'First Post Content',
+		'date_posted': 'Jan 05, 2020'
+	}
+]
 
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return HttpResponse('<h1>Blog About</h1>')
+	return render(request, 'blog/about.html', {'title': 'About'})
+ 
